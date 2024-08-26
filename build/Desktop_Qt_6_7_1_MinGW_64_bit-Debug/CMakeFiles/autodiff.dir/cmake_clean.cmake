b3495b98be5456dd7006d3c81d955d78958158e7
ArtFilippov <artfilipp0w@yandex.ru>
2024-08-26 05:03:27 +0500

    first commit

---
+++ build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/CMakeFiles/autodiff.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/autodiff_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/autodiff_autogen.dir/ParseCache.txt"
+  "autodiff_autogen"
+  "CMakeFiles/autodiff.dir/autodiff_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/autodiff.dir/autodiff_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/autodiff.dir/main.cpp.obj"
+  "CMakeFiles/autodiff.dir/main.cpp.obj.d"
+  "CMakeFiles/autodiff.dir/mainwindow.cpp.obj"
+  "CMakeFiles/autodiff.dir/mainwindow.cpp.obj.d"
+  "autodiff.exe"
+  "autodiff.exe.manifest"
+  "autodiff.pdb"
+  "autodiff_autogen/mocs_compilation.cpp"
+  "autodiff_autogen/timestamp"
+  "libautodiff.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/autodiff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
